a298f4902f1af7538d16e1892f6693646fc5688c
IntanNuraudhatul <81946507+IntanNuraudhatul@users.noreply.github.com>
2021-10-04 11:55:17 +0800

    Latihan Asas

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ASAS PHP </h1>
+
+    <?php
+    // komen satu baris
+    # komen satu baris
+    /*
+    komen banyak baris
+    */
+
+    $namapelajar = 'Ali';
+    echo "<h2>Hello $namapelajar</h2>; #output"
+
+    $hadir = false;
+    $umur = 20;
+    $nokp = '010513021384'
+
+    ?<
+    
+</body>
+</html>
